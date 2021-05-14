8562b92cb89754ed061921d952b189ac458bc387
kinderasteroid <84179566+kinderasteroid@users.noreply.github.com>
2021-05-14 16:22:21 +0530

    Git Commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-path = "C:\msedgedriver.exe"
+path = "Your Driver Path"
 driver = webdriver.Edge(path)
 args: ['--disable-web-security', '--user-data-dir', '--allow-running-insecure-content' ]
 driver.get("https://google.com")
@@ -10,17 +10,17 @@ s = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div/div/div[2]/a"
 s.click()
 email = driver.find_element_by_css_selector("#identifierId")
 args: ['--disable-web-security', '--user-data-dir', '--allow-running-insecure-content' ]
-email.send_keys("ssuuhhaass777@gmail.com")
+email.send_keys("Your Email Here")
 email.send_keys(Keys.ENTER)
 time.sleep(3)
 passu = driver.find_element_by_css_selector("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input")
-passu.send_keys("ssuuhhaass")
+passu.send_keys("Your Password")
 passu.send_keys(Keys.ENTER)
 time.sleep(3)
 args: ['--disable-web-security', '--user-data-dir', '--allow-running-insecure-content' ]
 driver.get("https://meet.google.com/?hs=197&pli=1&authuser=0")
 meet = driver.find_element_by_css_selector("#i3")
-meet.send_keys("tsmrufsfgp")
+meet.send_keys("Your Meeting Id")
 meet.send_keys(Keys.ENTER)
 time.sleep(5)
 driver.find_element_by_xpath("/html/body/div/div[3]/div/div[2]/div[3]/div/span/span").click()
